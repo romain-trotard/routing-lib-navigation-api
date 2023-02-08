01f55d24c435ad99f18520b79c975c2d8f226aeb
Romain Trotard <romain.trotard@gmail.com>
2023-02-08 13:11:11 +0100

    Change the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,19 @@
 import Layout from "../components/Layout";
-import useLoaderData from "../lib/userLoaderData";
 
 export default function HomePage() {
-    const loaderData = useLoaderData<string>();
-
     return (
         <Layout>
-            <p>Home</p>
-            <a href="/first">Go first page</a>
-            <p>Data: {loaderData}</p>
+            <p>This project uses a custom implementation of routing with the new <strong>Navigation Web API</strong>.</p>
+            <p>This implementation is abstract, you can find it under the function <code>createBrowserRouter</code>. It has the following features:</p>
+            <div className="featuresWrapper">
+                <ul>
+                    <li>Single Page Application</li>
+                    <li>Being able to do some navigation</li>
+                    <li>Implement loader data (like <strong>React Router v6</strong> and <strong>Tanstack Router</strong>)</li>
+                    <li>Prompt a modal when leaving a page with unsaved changes</li>
+                </ul>
+            </div>
+            <p>I used this implementation with <strong>React</strong>. But you could use it with any library/framework.</p>
         </Layout>
     );
 }
